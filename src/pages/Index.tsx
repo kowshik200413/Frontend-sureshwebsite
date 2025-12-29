0d488beacf967bc6af4bc2e67deda01ba80777c2
ansar <ansavali3231@gmail.com>
2025-12-29 20:39:53 +0530

    image added to background

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,9 @@
 import VoiceAgent from "@/components/VoiceAgent";
 
 export default function Index() {
-  return <VoiceAgent />;
+  return (
+    <div className="h-screen w-full bg-contain bg-no-repeat bg-center bg-[url('/Screenshot%202025-12-29%20203429.png')]">
+      <VoiceAgent />
+    </div>
+  );
 }
